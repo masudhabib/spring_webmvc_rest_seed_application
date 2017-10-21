9a7121f4662b590620472abf5c783858865f6623
masudhabib <masudhabib@users.noreply.github.com>
2017-10-21 11:55:40 -0400

    remove deprecated webmvcconfigureradapter
    as WebMvcConfigurerAdapter was deprecated, but for java 8 WebMvcConfigurer can be directly used

--- src/main/java/com/seedapp/springrest/config/WebAppConfig.java
+++ src/main/java/com/seedapp/springrest/config/WebAppConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @ComponentScan("com.seedapp.springrest.webmvc.rest")
 @EnableWebMvc
-public class WebAppConfig extends WebMvcConfigurerAdapter {
-
+//public class WebAppConfig extends WebMvcConfigurerAdapter { //commented out as WebMvcConfigurerAdapter was deprecated for java 8 WebMvcConfigurer can be directly used
+public class WebAppConfig implements WebMvcConfigurer {
 	@Override
 	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
 		configurer.enable();
